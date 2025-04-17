de9ed0ca6055bfbcf800c8e2fa686af8ac8996a2
YMY arctto <2974466588@qq.com>
2025-04-17 20:44:00 +0800

    feat(scripts)：添加效果词条以及药材SO文件

---
+++ Scripts/Models/Pharmacy/MedicinalMaterial/MedicinalMaterial_SO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MedicinalMaterial_SO", menuName = "Models/MedicinalMaterial_SO", order = 1)]
+public class MedicinalMaterial_SO : ScriptableObject
+{
+    [Header("药材名称")]public string Name;
+    [SerializeField][Header("药材功效")]public List<Efficacy> Efficacy;
+    [SerializeField][Header("药材副作用")]public List<SideEffect> SideEffect;
+    [Header("药材描述")]public string Description;
+}
