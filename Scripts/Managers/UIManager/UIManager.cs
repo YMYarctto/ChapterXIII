948853b53f638c304c76afaa7cf6e03e21b509fe
YMY arctto <2974466588@qq.com>
2025-04-17 12:22:40 +0800

    init(manager)：导入manager脚本

---
+++ Scripts/Managers/UIManager/UIManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("启用Debug模式")]
+    public bool DebugModel;
+    private Dictionary<string,UIView> _UIs;
+
+    private static UIManager _UIManager;
+    public static UIManager instance
+    {
+        get
+        {
+            if (!_UIManager)
+            {
+                _UIManager = FindObjectOfType(typeof(UIManager)) as UIManager;
+                if (!_UIManager)
+                {
+                    Debug.LogWarning("UIManager:\n没有找到UIManager,请在场景中添加一个UIManager");
+                    return null;
+                }
+                _UIManager.Init();
+            }
+            return _UIManager;
+        }
+    }
+    void Init()
+    {
+        _UIs ??= new();
+    }
+
+    public void AddUIView(string url, UIView view)
+    {
+        if (_UIs.ContainsKey(url))
+        {
+            Debug.LogError($"UIManager:\n添加Url: \"{url}\" 失败,该Url已存在");
+            return;
+        }
+        _UIs.Add(url, view);
+        view.Init();
+
+    }
+    public void RemoveUIView(params string[] urls)
+    {
+        foreach(var u in urls){
+            _UIs.Remove(u);
+        }
+    }
+    public void EnableUIView(string url)
+    {
+        if (!_UIs.ContainsKey(url))
+        {
+            Debug.LogError($"UIManager:\n启用Url: \"{url}\" 失败,该Url不存在");
+            return;
+        }
+        _UIs[url].Enable();
+    }
+    public void DisableUIView(string url)
+    {
+        if (!_UIs.ContainsKey(url))
+        {
+            Debug.LogError($"UIManager:\n禁用Url: \"{url}\" 失败,该Url不存在");
+            return;
+        }
+        _UIs[url].Disable();
+    }
+}
