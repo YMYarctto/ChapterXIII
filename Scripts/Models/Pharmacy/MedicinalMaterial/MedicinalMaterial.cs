98e73a7b7e1d7034cc5342d3c8d106549eeb9480
YMY arctto <2974466588@qq.com>
2025-04-17 22:07:58 +0800

    feat(MedicinalMaterial): 实现药材放入炼药锅并反馈debug

--- Scripts/Models/Pharmacy/MedicinalMaterial/MedicinalMaterial.cs
+++ Scripts/Models/Pharmacy/MedicinalMaterial/MedicinalMaterial.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MedicinalMaterial : MonoBehaviour
+public class MedicinalMaterial : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("")]public MedicinalMaterial_SO medicinalMaterial_SO;
+    Transform parent;
+
+    void Awake() {
+        parent = GameObject.Find("Top_MousePointer").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    GameObject thisGameObject;
+    
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        thisGameObject = Instantiate(gameObject, parent);
+        thisGameObject.GetComponent<MedicinalMaterial>().medicinalMaterial_SO = medicinalMaterial_SO;
+        thisGameObject.transform.position = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        thisGameObject.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Destroy(thisGameObject);
+        // Vector2 Position = Camera.main.ScreenToWorldPoint(eventData.position);
+        RaycastHit2D inner = Physics2D.Raycast(eventData.position, Vector2.zero);
+        // 不为null，则认为有物体撞到
+        if (inner.collider!=null&&inner.collider.CompareTag("Pot"))
+        {
+            Debug.Log("药材放入药锅");
+            // EventManager.instance.SetInvokeParam("Pot/Add",medicinalMaterial_SO);
+            // EventManager.instance.Invoke("Pot/Add");
+        }
+
     }
 }
--- Scripts/Models/Pharmacy/MedicinalMaterial/MedicinalMaterial_SO.cs
+++ Scripts/Models/Pharmacy/MedicinalMaterial/MedicinalMaterial_SO.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MedicinalMaterial_SO", menuName = "Models/MedicinalMaterial_SO", order = 1)]
 public class MedicinalMaterial_SO : ScriptableObject
 {
+    [Header("药材ID")]public uint ID;
     [Header("药材名称")]public string Name;
     [SerializeField][Header("药材功效")]public List<Efficacy> Efficacy;
     [SerializeField][Header("药材副作用")]public List<SideEffect> SideEffect;
-    [Header("药材描述")]public string Description;
+    [TextArea(1,5)][Header("药材描述")]public string Description;
 }
