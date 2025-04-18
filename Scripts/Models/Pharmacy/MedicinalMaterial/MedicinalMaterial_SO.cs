06d374228dc4d5b99118d88ab5b6b50451c80572
YMY arctto <2974466588@qq.com>
2025-04-18 14:24:28 +0800

    feat(Button): 导入字体资源，测试按钮交互效果

--- Scripts/Models/Pharmacy/MedicinalMaterial/MedicinalMaterial_SO.cs
+++ Scripts/Models/Pharmacy/MedicinalMaterial/MedicinalMaterial_SO.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ETag;
 
 [CreateAssetMenu(fileName = "MedicinalMaterial_SO", menuName = "Models/MedicinalMaterial_SO", order = 1)]
 public class MedicinalMaterial_SO : ScriptableObject
---
+++ Scripts/UIViews/MainMenu/Button/Button_Start.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class Button_Start : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    TMP_Text text;
+
+    void Awake()
+    {
+        text= GetComponentInChildren<TMP_Text>();
+        if (text == null)
+        {
+            Debug.LogError("Button: 未找到 TMP_Text 组件");
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ColorUtility.TryParseHtmlString("#F01d48", out Color color);
+        text.color = color;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = Color.black;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //TODO: 点击事件
+    }
+}
