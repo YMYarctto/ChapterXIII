f3bb11789b1251da1f029ae318ae714dbbe65143
YMY arctto <2974466588@qq.com>
2025-04-18 16:14:18 +0800

    fix(MedicinalMaterial): 将名称用枚举保存

---
+++ Scripts/Models/Enum/EMaterial.cs
@@ -0,0 +1,16 @@
+namespace EMaterial
+{
+    public enum MaterialName
+    {
+        绵眠叶=1,
+        银须菌,
+        凉影果,
+        石心花,
+        骨响草,
+        灰胆根,
+        月落藤,
+        虹须萝卜=13,
+        烈根生姜,
+        迷灵薄荷,
+    }
+}
